c0ddd6fe84e07c9a6ab98abc50b8c4312e301657
rhinodavid <dawalsh@gmail.com>
2016-09-30 16:19:22 -0700

    Add YouTube API GET helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    cache: false,
+    data: {
+      part: 'snippet',
+      type: 'video',
+      videoEmbeddable: 'true',
+      maxResults: options.max || 5,
+      q: options.query,
+      key: options.key
+    },
+    success: (data) => {
+      callback(data.items);
+    },
+    error: (err) => {
+      console.error(err);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
