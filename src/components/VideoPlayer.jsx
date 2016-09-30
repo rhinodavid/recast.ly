c12daa0c5271477858139344bd3be674833c541d
rhinodavid <dawalsh@gmail.com>
2016-09-30 14:57:06 -0700

    Passing VideoPlayer

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,11 @@
-var VideoPlayer = () => (
+var VideoPlayer = (params) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${params.video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{params.video.snippet.title}</h3>
+      <div>{params.video.snippet.description}</div>
     </div>
   </div>
 );
